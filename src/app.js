b22b712794f1a6dfc1d0c116e2f6b90a3342d09a
RanjeeshTech <ranjeeshr07012002@gmail.com>
2021-05-14 19:34:27 +0530

    Solved

--- src/app.js
+++ src/app.js
@@ -3,14 +3,40 @@
 //Debug the type of data provided
 //Return the types concatenated in a single variable
 
+//moreAboutHome() Function with three arguments passed
+let moreAboutHome = (address,distanceFromTown,hasNeighbours)=>{
+    //concatenatedString variable returns the concatenated of the type of three arguments
+    let concatenatedString = `${typeof(address)}${typeof(distanceFromTown)}${typeof(hasNeighbours)}`
+    return concatenatedString   
+}
+
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+
+let moreAboutKaren = (parents,noOfSiblings,isNuclearFamily)=>{
+    let concatenatedString = `${typeof(parents)}${typeof(noOfSiblings)}${typeof(isNuclearFamily)}`
+    if(concatenatedString==="stringnumberboolean")
+    return true
+    else 
+    return false
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+let doesFriendExist = (ageInText,ageInNumber)=>{
+    if(isNaN(ageInText)==true&&isNaN(ageInNumber)==false)
+    return NaN   
+    else if(isNaN(ageInText)==true&&isNaN(ageInNumber)==true&&ageInText!="NaN")
+    return NaN
+    else if(ageInText=="NaN"&&isNaN(ageInNumber)==true)
+    return "NaN"
+    else if(isNaN(ageInText)==false&&isNaN(ageInNumber)==true)
+    return NaN
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -19,9 +45,28 @@
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+let sweetTooth = (totalNoOfSweets,sweetsConsumedByKaren,sweetsConsumedInNMeters,metersToTravel)=>{
+    let concatenatedString = `${typeof(totalNoOfSweets)}${typeof(sweetsConsumedByKaren)}${typeof(sweetsConsumedInNMeters)}${typeof(metersToTravel)}`
+    if(concatenatedString=="numbernumbernumbernumber"){
+        return 0
+    }
+    else{
+        return "No sweets for Karen's friend"
+    }
+}
+
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+
+let convertToCelsius = (fahrenheit)=>{
+    if(typeof fahrenheit=="string"||typeof fahrenheit=="object"||typeof fahrenheit=="undefined"){
+        return "Technical Error!"     
+    }
+    else{        
+        return (fahrenheit-32)*5/9
+    }
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -30,6 +75,21 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+
+let aDifficultChoice = (choice)=>{
+    if(choice==1){
+        return "Take her daughter to a doctor"
+    }
+    else if(choice==-1){
+        return "Break down and give up all hope"
+    }
+    else if(typeof choice === "undefined"){
+        return "Wasn't able to decide"
+    }
+    else if(choice=="I give up"){
+        return "Refused to do anything for Karen"
+    }
+}
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
